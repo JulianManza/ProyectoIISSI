28b8295f5c4c86ce3ec07f9c48554d8687992584
julianmanza <josmanmon@alum.us.es>
2018-09-18 11:41:58 +0200

    Fix Validacion PHP

--- trunk/web-iissi/registrarCliente.php
+++ trunk/web-iissi/registrarCliente.php
@@ -1,11 +1,11 @@
 <?php
-   session_start();
-   require_once("gestionBD.php");
-   require_once("gestionarDireccion.php");
-   
-   if (isset($_SESSION["formulario"])) {
-		// Recogemos los datos del formulario
-		$nuevoUsuario["nif"] = $_REQUEST["nif"];
+session_start();
+require_once("gestionBD.php");
+require_once("gestionarDireccion.php");
+$conexion = crearConexionBD();
+// Comprobar que hemos llegado a esta página porque se ha rellenado el formulario
+if (isset($_SESSION["formulario"])) {
+ 		$nuevoUsuario["nif"] = $_REQUEST["nif"];
 		$nuevoUsuario["nombre"] = $_REQUEST["nombre"];
 		$nuevoUsuario["apellidos"] = $_REQUEST["apellidos"];
 		$nuevoUsuario["calle"] = $_REQUEST["calle"];
@@ -13,88 +13,75 @@
 		$nuevoUsuario["provincia"] = $_REQUEST["provincia"];
 		$nuevoUsuario["email"] = $_REQUEST["email"];
 		$nuevoUsuario["pass"] = $_REQUEST["pass"];
-		$nuevoUsuario["confirmpass"] = $_REQUEST["confirmpass"];	
-	}else {// En caso contrario, vamos al formulario
-		Header("Location: registro.php");
-	}
-	// Guardar la variable local con los datos del formulario en la sesión.
+		$nuevoUsuario["confirmpass"] = $_REQUEST["confirmpass"];
+		
 	$_SESSION["formulario"] = $nuevoUsuario;
-	// Validamos el formulario en servidor
-	// Si se produce alguna excepción PDO en la validación, volvemos al formulario informando al usuario
-	try{ 
-		$conexion = crearConexionBD(); 
-		$errores = validarDatosUsuario($conexion, $nuevoUsuario);
-		cerrarConexionBD($conexion);
-	}catch(PDOException $e){
-		// Mensaje de depuración
-		$_SESSION["errores"] = "<p>ERROR en la validación: fallo en el acceso a la base de datos.</p><p>"
-		 . $e->getMessage() . "</p>";
-		Header('Location: registro.php');
-	}
 	
-	// Si se han detectado errores
-	if (count($errores)>0) {
-		// Guardo en la sesión los mensajes de error y volvemos al formulario
-		$_SESSION["errores"] = $errores;
-		Header('Location: registro.php');
-	} else
-		// Si todo va bien, vamos a la página de éxito (inserción del usuario en la base de datos)
-		Header('Location: exitoRegistro.php');
-
-///////////////////////////////////////////////////////////
+	$errores = validarDatosUsuario($conexion, $nuevoUsuario);
+	
+	if (count($errores)> 0) {	
+	$_SESSION["errores"] = $errores;
+	Header("Location:registro.php");
+	}else{	
+	Header("Location: exitoRegistro.php");
+	}}
+cerrarConexionBD($conexion);
+/////////////////// EJERCICIO 1
 // Validación en servidor del formulario de alta de usuario
-///////////////////////////////////////////////////////////
-function validarDatosUsuario($conexion, $nuevoUsuario){
-	$errores=array();
-	// Validación del NIF
-	if($nuevoUsuario["nif"]=="") 
-		$errores[] = "<p>El NIF no puede estar vacío</p>";
-	else if(!preg_match("/^[0-9]{8}[A-Z]$/", $nuevoUsuario["nif"])){
-		$errores[] = "<p>El NIF debe contener 8 números y una letra mayúscula: " . $nuevoUsuario["nif"]. "</p>";
+function validarDatosUsuario($conexion, $nuevoUsuario) {
+	$errores = array();
+	// Validación del Nombre
+	if (empty($nuevoUsuario["nombre"])){
+		$errores[] = "<p> El Nombre no puede estar vacío.</p>";
 	}
-
-	// Validación del Nombre			
-	if($nuevoUsuario["nombre"]=="") 
-		$errores[] = "<p>El nombre no puede estar vacío</p>";
+	// Validación del NIF
+	if (empty($nuevoUsuario["nif"])){
+		$errores[] = "<p> El NIF no puede estar vacío.</p>";
+	}else if (!preg_match("/^[0-9]{8}[A-Z]/", $nuevoUsuario["nif"])){
+		$errores[] = "<p> El NIF debe estar compuesto por 8 numeros seguido de una letra.</p>";
+	}
+		
 	
 	// Validación del email
-	if($nuevoUsuario["email"]==""){ 
-		$errores[] = "<p>El email no puede estar vacío</p>";
-	}else if(!filter_var($nuevoUsuario["email"], FILTER_VALIDATE_EMAIL)){
-		$errores[] = "<p>El email es incorrecto: " . $nuevoUsuario["email"]. "</p>";
+	if (empty($nuevoUsuario["email"])){
+		$errores[] = "<p> El Correo electrónico no puede estar vacio.</p>";
+	}else if (!filter_var($nuevoUsuario["email"],FILTER_VALIDATE_EMAIL)){
+		$errores[] = "<p> Coreo electrónico no valido.</p>";
 	}
 	// Validación de la contraseña
-	if(!isset($nuevoUsuario["pass"]) || strlen($nuevoUsuario["pass"])<8){
-		$errores [] = "<p>Contraseña no válida: debe tener al menos 8 caracteres</p>";
-	}else if(!preg_match("/[a-z]+/", $nuevoUsuario["pass"]) || 
-		!preg_match("/[A-Z]+/", $nuevoUsuario["pass"]) || !preg_match("/[0-9]+/", $nuevoUsuario["pass"])){
-		$errores[] = "<p>Contraseña no válida: debe contener letras mayúsculas y minúsculas y dígitos</p>";
-	}else if($nuevoUsuario["pass"] != $nuevoUsuario["confirmpass"]){
-		$errores[] = "<p>La confirmación de contraseña no coincide con la contraseña</p>";
+	if(empty($nuevoUsuario["pass"])){
+		$errores[] = "<p> La contraseña no puede estar vacía.</p>";
+	}else{
+	if (strlen($nuevoUsuario["pass"]<8)) 
+		$errores[] = "<p> La contraseña debe tener al menos 8 caracteres.</p>";
+	if (!preg_match("/[A-Za-z]/", $nuevoUsuario["pass"])||
+	!preg_match("/[0-9]/", $nuevoUsuario["pass"]))
+	$errores[] = "<p> La contraseña debe contener al menos una letra mayuscula o minuscula y al menos un digito.</p>";
+	
+	if($nuevoUsuario["pass"] != $nuevoUsuario["confirmpass"])
+	$errores[] = "<p> La contraseña y su confirmacion deben de ser iguales.</p>";
 	}
 	// Validación de la dirección
-	if($nuevoUsuario["calle"]==""){
-		$errores[] = "<p>La dirección no puede estar vacía</p>";	
-	}
-	// Validar municipio y provincia
+	if (empty($nuevoUsuario["calle"]))
+		$errores[] = "<p> La calle no puede estar vacía.</p>";
+	
+	
 	$error = validarProvinciaMunicipio($conexion, $nuevoUsuario["provincia"], $nuevoUsuario["municipio"]);
-	if($error!="")
-		$errores[] = $error;
-	return $errores;	
+		if ($error != "") {
+			$errores[] = $error;
+		}
+	return $errores;
 }
-	// Comprueba que la pareja municipio-provincia están en la BD
 function validarProvinciaMunicipio($conexion, $provincia, $municipio){
 	$error="";
-	$mun = buscarMunicipioProvincia($conexion, $provincia, $municipio);
+		$mun = buscarMunicipioProvincia($conexion, $provincia, $municipio);
 	$cont = 0;
 	foreach($mun as $m){
 		$cont = $cont + 1;
 	}
-	
 	if($cont != 1){
 		$error =  "<p>El municipo y la provincia no son válidos</p>";
 	}
 	return $error;
 }
-	
 ?>
--- trunk/web-iissi/registro.php
+++ trunk/web-iissi/registro.php
@@ -1,94 +1,84 @@
 <?php
-	session_start();
-	// Importar librerías necesarias para gestionar direcciones y géneros literarios
-	require_once("gestionBD.php");
-	require_once("gestionarDireccion.php");
-	if (isset($_SESSION['login']))
-		header("Location: logout.php");
+session_start();
+require_once ("gestionBD.php");
+require_once ("gestionarDireccion.php");
+if(isset($_SESSION['logueado'])){
+	unset($_SESSION['login']);
+	unset($_SESSION['logueado']);
+}
+if (!isset($_SESSION["formulario"])) {
+	$formulario["nif"] = " ";
+	$formulario["nombre"] = " ";
+	$formulario["apellidos"] = " ";
+	$formulario["calle"] = " ";
+	$formulario["municipio"] = " ";
+	$formulario["provincia"] = " ";
+	$formulario["email"] = " ";
+	$formulario["pass"] = " ";
+	$formulario["confirmpass"] = " ";
 	
-	if (!isset($_SESSION['formulario'])) {
-		$formulario['nif'] = "";
-		$formulario['nombre'] = "";
-		$formulario['apellidos'] = "";
-		$formulario['calle'] = "";
-		$formulario['municipio'] = "";
-		$formulario['provincia'] = "";
-		$formulario['email'] = "";
-		$formulario['pass'] = "";
-	
-		$_SESSION['formulario'] = $formulario;
-	}
-	else{
-		$formulario = $_SESSION['formulario'];
-		
-	}
-	
-	if (isset($_SESSION['errores'])){
-		$errores = $_SESSION['errores'];
-		unset($_SESSION['errores']);	
-	}
-	// Creamos una conexión con la BD
-	$conexion = crearConexionBD();
-	
-
+	$_SESSION["formulario"] = $formulario;
+} else {
+	$formulario = $_SESSION["formulario"];
+}
+if (isset($_SESSION["errores"])){
+	$errores = $_SESSION["errores"];
+	unset($_SESSION["errores"]);
+}
+$conexion = crearConexionBD()
 ?>
 
 <!DOCTYPE html>
-<html>
-  <head>
-	<meta charset="UTF-8">
-	<link rel="stylesheet" type="text/css" href="css/plantilla.css" />
-	<script src="https://code.jquery.com/jquery-3.1.1.min.js" type="text/javascript"></script>
-	<script src="js/validacion_cliente_alta_usuario.js" type="text/javascript"></script>
-	<title> Gestion de clientes: Alta de Clientes</title>
+<html lang="es">
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="css/plantilla.css" />
+		<script src="https://code.jquery.com/jquery-3.1.1.min.js" type="text/javascript"></script>
+		<script src="js/validacion_cliente_alta_usuario.js" type="text/javascript"></script>
+		<title>Saind: Gestion de Usuarios</title>
+	</head>
 
-  </head>
-
-  <body>
+	<body>
 		<script>
 		// Inicialización de elementos y eventos cuando el documento se carga completamente
-		$(document).ready(function() {
+			$(document).ready(function() {
 
-			// EJERCICIO 3: Manejador de evento del color de la contraseña
-			$("#pass").on("keyup", function() {
-				// Calculo el color
-				passwordColor();
-			});
-
-			// EJERCICIO 4: Uso de AJAX con JQuery para cargar de manera asíncrona los municipios según la provincia seleccionada
-			// Manejador de evento sobre el campo de provincias
-			$("#provincia").on("input", function () {
-				// Llamada AJAX con JQuery, pasándole el valor de la provincia como parámetro
-        		$.get("cambio_provincia.php", { provinciaMunicipio: $('#provincia').val()}, function (data) {
-        			// Borro los municipios que hubiera antes en el datalist
-        			$("#opcionesMunicipios").empty();
-        			// Adjunto al datalist la lista de municipios devuelta por la consulta AJAX
-        			$("#opcionesMunicipios").append(data);
+				// EJERCICIO 3: Manejador de evento del color de la contraseña
+				$("#pass").on("keyup", function() {
+					// Calculo el color
+					passwordColor();
 				});
-    		});
-		});
-	</script>
+
+				// EJERCICIO 4: Uso de AJAX con JQuery para cargar de manera asíncrona los municipios según la provincia seleccionada
+				// Manejador de evento sobre el campo de provincias
+				$("#provincia").on("input", function() {
+					// Llamada AJAX con JQuery, pasándole el valor de la provincia como parámetro
+					$.get("cambio_provincia.php", {
+						provinciaMunicipio : $('#provincia').val()
+					}, function(data) {
+						// Borro los municipios que hubiera antes en el datalist
+						$("#opcionesMunicipios").empty();
+						// Adjunto al datalist la lista de municipios devuelta por la consulta AJAX
+						$("#opcionesMunicipios").append(data);
+					});
+				});
+			});
+		</script>
 		
 		<?php
-			include_once("cabecera.php");
-			include_once("menu.php");
+		include_once("cabecera.php");
+		include_once("menu.php");
+		
+
+if (isset($errores))	{
+echo "<div class='error'>";
+foreach ($errores as $error) echo $error;
+echo "</div>";
+}	
 		?>
-		
-		
-			<?php 
-		// Mostrar los erroes de validación (Si los hay)
-		if (isset($errores) && count($errores)>0) { 
-	    	echo "<div id=\"div_errores\" class=\"error\">";
-			echo "<h4> Errores en el formulario:</h4>";
-    		foreach($errores as $error){
-    			echo $error;
-			} 
-    		echo "</div>";
-  		}
-	?>
-		
-  <form id="altaUsuario" name="altaUsuario" method="get" action="registrarCliente.php" onsubmit="return validateForm()">
-  		<p><i>Los campos obligatorios están marcados con </i><em>*</em></p>
+
+		<form id="altaUsuario" method="get" action="registrarCliente.php" novalidate>
+			  		<p><i>Los campos obligatorios están marcados con </i><em>*</em></p>
   		<fieldset id="datosFormu" name="datosFormu" class="datosFormu">
   			
     <div class="nameUser"><label for="nombre">Nombre:<em>*</em></label>
@@ -99,27 +89,27 @@
 			<input id="apellidos" name="apellidos" type="text" size="80" value="<?php echo $formulario['apellidos'];?>"/>
 	</div>
 	
-	<div><label for="nif">NIF<em>*</em></label>
+	<div><label for="nif">NIF	<em>*	</em></label>
 			<input id="nif" name="nif" type="text" placeholder="12345678X" pattern="^[0-9]{8}[A-Z]" title="Ocho dígitos seguidos de una letra mayúscula" value="<?php echo $formulario['nif'];?>" required>
 	</div>
 
-    <div><label for="email">Email:<em>*</em></label>
+    <div><label for="email">Email:	<em>*</em></label>
 			<input id="email" name="email"  type="email" placeholder="usuario@dominio.extension" value="<?php echo $formulario['email'];?>" required/><br>
 	</div>
 	
-    <div><label for="pass">Password:<em>*</em></label>
+    <div><label for="pass">Password:	<em>*</em></label>
             <input type="password" name="pass" id="pass" placeholder="Mínimo 8 caracteres entre letras y dígitos" required oninput="passwordValidation(); "/>
 	</div>
 	
-    <div><label for="confirmpass">Confirmar Password: </label>
+    <div><label for="confirmpass">Confirmar Password:	</label>
 			<input type="password" name="confirmpass" id="confirmpass" placeholder="Confirmación de contraseña"  oninput="passwordConfirmation();" required"/>
 	</div>
 
-	<div><label for="calle">Calle/Avda.:<em>*</em></label>
+	<div><label for="calle">Calle/Avda.:	<em>*</em></label>
 			<input id="calle" name="calle" type="text" size="80" value="<?php echo $formulario['calle'];?>" required/>
 	</div>
 
-	<div><label for="provincia">Provincia:<em>*</em></label>
+	<div><label for="provincia">Provincia:	<em>*</em></label>
 			<input list="opcionesProvincias" name="provincia" id="provincia" required value="<?php echo $formulario['provincia'];?>"/>
 			<datalist id="opcionesProvincias">
 			  	<?php
@@ -132,21 +122,18 @@
 			</datalist>
 	</div>
 
-	<div><label for="municipio">Municipio:<em>*</em></label>
+	<div><label for="municipio">Municipio:	<em>*</em></label>
 			<input id="municipio" name="municipio" type="text" list="opcionesMunicipios" required value="<?php echo $formulario['municipio'];?>">
 			<datalist id="opcionesMunicipios">
 			</datalist>
 	</div>
 	<div class="button" >
-        <button type="submit">Registrar</button>
+        <input type="submit" value="Registrar" />
     </div>
     </fieldset>
-</form>
+		</form>
 		<?php
-			include_once("pie.php");
-			cerrarConexionBD($conexion);
+		include_once ("pie.php");
 		?>
-
-  </body>
-
+	</body>
 </html>
